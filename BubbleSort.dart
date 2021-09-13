0b1f84e06e67e578566b48273af80e20aa74c815
Russell Banks <Russell@bandev.uk>
2021-09-13 23:27:25 +0100

    Add Dart bubble sort

---
+++ BubbleSort.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+List<int> bubbleSort(List<int> list) {
+  var retList = new List<int>.from(list);
+  var tmp;
+  var swapped = false;
+  do {
+    swapped = false;
+    for (var i = 1; i < retList.length; i++) {
+      if (retList[i - 1] > retList[i]) {
+        tmp = retList[i - 1];
+        retList[i - 1] = retList[i];
+        retList[i] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return retList;
+}
+
+void main() {
+  final random = new Random();
+  final min = 1;
+  final max = 100;
+  List<int> array = [];
+  for (int i = 0; i < 5; i++) {
+    array.add(min + random.nextInt(max - min));
+  }
+  print(array);
+  List<int> sortedarray = bubbleSort(array);
+  print(sortedarray);
+}
