cd43a48ed1347691e997b33da86ddc089623c75d
Jack Devey <jack@bandev.uk>
2021-09-13 20:19:11 +0100

    Add TypeScript bubble sort (#2)

---
+++ BubbleSort.ts
@@ -0,0 +1,22 @@
+function bubbleSort(array: number[]): number[] {
+     
+    for(var i = 0; i < array.length; i++){
+        
+      for(var j = 0; j < ( array.length - i -1 ); j++){
+          
+        if(array[j] > array[j+1]){
+            
+          var temp: number = array[j]
+          array[j] = array[j + 1]
+          array[j+1] = temp
+        }
+      }
+    }
+
+    return array
+   }
+
+   var unsortedArray: number[] = Array.from({length: 5}, () => Math.floor(Math.random() * 100))
+
+   console.log("Unsorted array:", unsortedArray)
+   console.log("Sorted array:", bubbleSort(unsortedArray))
