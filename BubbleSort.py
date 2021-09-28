8c2f77998450355b6e844f152f5010dc01277066
Russell Banks <Russell@bandev.uk>
2021-09-28 15:30:18 +0100

    Improve formatting of Python bubble sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,18 +1,20 @@
 import random
 
-def bubbleSort(array):
+
+def bubblesort(array):
     n = len(array)
-  
-    for i in range(n-1):
-  
-        for j in range(0, n-i-1):
-  
-            if array[j] > array[j + 1] :
+
+    for i in range(n - 1):
+
+        for j in range(0, n - i - 1):
+
+            if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
 
     return array
-  
-unsortedArray = [random.randint(1,100) for _ in range(5)]
-  
-print ("Unsorted array:", unsortedArray)
-print("Sorted array:", bubbleSort(unsortedArray))
+
+
+unsortedArray = [random.randint(1, 100) for _ in range(5)]
+
+print("Unsorted array:", unsortedArray)
+print("Sorted array:", bubblesort(unsortedArray))
